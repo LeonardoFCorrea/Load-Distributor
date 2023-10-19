7eb5553e942795a8ead7a0b4b0f3514a1760ce84
ryancs87 <ryancovattis@gmail.com>
2023-10-19 13:43:59 -0300

    adicionado round robin e least response time

--- balanceamento.py
+++ balanceamento.py
@@ -1,16 +1,65 @@
 import socket
+import time
+import random
 
-HOST = '0.0.0.0'  
-PORT = 8080      
+# Lista de endereços IP das máquinas virtuais de destino
+destinos = ['192.168.1.2', '192.168.1.3', '192.168.1.4']  # Substitua pelos IPs reais das máquinas virtuais
+destinos_tempo_resposta = {}  # Dicionário para rastrear o tempo de resposta de cada destino
 
-destinos = ['192.168.1.2', '192.168.1.3']  
+# Porta para encaminhar as solicitações
+PORT = 8080
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((HOST, PORT))
-server_socket.listen(5)
+def round_robin():
+    current_index = 0
 
-print(f"Servidor de balanceamento ouvindo na porta {PORT}")
+    while True:
+        yield destinos[current_index]
+        current_index = (current_index + 1) % len(destinos)
 
-while True:
-    client_socket, client_address = server_socket.accept()
-    print(f"Conexão recebida de {client_address}")
+def least_response_time():
+    while True:
+        destinos.sort(key=lambda destino: destinos_tempo_resposta.get(destino, 0))
+        yield destinos[0]
+
+def main(method):
+    if method == "round-robin":
+        dest_selector = round_robin()
+    elif method == "least-response-time":
+        dest_selector = least_response_time()
+    else:
+        raise ValueError("Método de balanceamento de carga desconhecido")
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('0.0.0.0', PORT))
+    server_socket.listen(5)
+
+    print(f"Servidor de balanceamento ({method}) ouvindo na porta {PORT}")
+
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print(f"Conexão recebida de {client_address}")
+
+        try:
+            destino_atual = next(dest_selector)
+
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as destino_socket:
+                start_time = time.time()
+                destino_socket.connect((destino_atual, PORT))
+                data = client_socket.recv(1024)
+                destino_socket.sendall(data)
+                response = destino_socket.recv(1024)
+                client_socket.sendall(response)
+                end_time = time.time()
+                tempo_resposta = end_time - start_time
+                destinos_tempo_resposta[destino_atual] = tempo_resposta
+
+        except Exception as e:
+            print(f"Erro ao encaminhar a solicitação: {e}")
+
+        client_socket.close()
+
+if __name__ == "__main__":
+    # Escolha o método de balanceamento de carga desejado aqui ("round-robin" ou "least-response-time")
+    metodo_balanceamento = "round-robin"  
+
+    main(metodo_balanceamento)
